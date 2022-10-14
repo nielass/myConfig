94f66aebacc89bb39e57d34bec19fd288e3965d4
Nielass <jdchen_97@outlook.com>
2022-10-14 23:06:49 +0200

    add K and pyright setting

--- lua/lsp/server/pyright.lua
+++ lua/lsp/server/pyright.lua
@@ -1,10 +1,16 @@
 return {
   on_setup = function(server)
     server.setup({
+      -- cmd = { "pyright-langserver", "--stdio", "-p", "./.pyright.json" },
       flags = {
         debounce_text_changes = 150,
       },
       on_attach = require('keybinding').on_attach,
+      python = {
+        analysis = {
+          typeCheckingMode= "off",
+        },
+      },
     })
   end,
 }
